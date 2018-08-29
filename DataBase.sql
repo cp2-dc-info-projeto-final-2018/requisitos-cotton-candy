9c23bd1aaa381f41fceaab9c7d1f47eb233fea5a
LeFilgueiras <filgueiras.leticiac@gmail.com>
2018-08-29 22:00:52 +0900

    Add files via upload

--- DataBase.sql
+++ DataBase.sql
@@ -16,8 +16,7 @@ CREATE TABLE TarefaEvento(
 
 CREATE TABLE Avaliacao(
 	id_Avaliacao INT PRIMARY KEY NOT NULL,
-	Tipo VARCHAR(100) NOT NULL,
-	id_Nota INT CONSTRAINT fk_Nota FOREIGN KEY REFERENCES Nota(id_Nota));
+	Tipo VARCHAR(100) NOT NULL);
 
 CREATE TABLE Materia(
 	id_Materia INT PRIMARY KEY NOT NULL,
@@ -26,11 +25,11 @@ CREATE TABLE Materia(
 	id_Aluno INT CONSTRAINT fk_Aluno FOREIGN KEY REFERENCES Aluno(id_Aluno));
 
 CREATE TABLE MateriaAvaliacao(
-	id_MateriaAv INT PRIMARY KEY NOT NULL,
 	id_Avaliacao INT CONSTRAINT fk_Avaliacao FOREIGN KEY REFERENCES Avaliacao(id_Avaliacao),
 	id_Materia INT CONSTRAINT fk_Materia FOREIGN KEY REFERENCES Materia(id_Materia));
 
 CREATE TABLE Nota(
 	id_Nota INT PRIMARY KEY NOT NULL,
 	Valor FLOAT,
-	id_Avaliacao INT CONSTRAINT fk_Avaliacao FOREIGN KEY REFERENCES Avaliacao(id_Avaliacao));
+	id_Avaliacao INT CONSTRAINT fk_Avaliacao FOREIGN KEY REFERENCES Avaliacao(id_Avaliacao),
+	id_Materia INT CONSTRAINT fk_Materia FOREIGN KEY REFERENCES Materia(id_Materia));
