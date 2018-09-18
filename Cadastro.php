ee217cf6e2a55ac357c7f9e7412b53cc56386d8b
LeFilgueiras <filgueiras.leticiac@gmail.com>
2018-09-18 21:23:17 +0900

    Add files via upload

--- Cadastro.php
+++ Cadastro.php
@@ -1,6 +1,26 @@
 <?php
+  include 'ConexaoBd.php';
+  function InsereUsuario($dadosNovoContato)
+  {
+    $bd = CriaConexãoBD();
 
+    $sql = $bd->prepare("INSERT INTO Usuario (nome, sobrenome, usuario, email, senha, instituicao) VALUES (:valnome, :valsobrenome, :valusuario, :valemail, :valsenha, :valinstituicao)");
+
+      $sql->bindValue(':valnome', $dadosNovoContato['nome']);
+      $sql->bindValue(':valsobrenome', $dadosNovoContato['sobrenome']);
+      $sql->bindValue(':valusuario', $dadosNovoContato['usuario']);
+      $sql->bindValue(':valemail', $dadosNovoContato['email']);
+      $sql->bindValue(':valsenha', $dadosNovoContato['senha']);
+      $sql->bindValue(':valinstituicao', $dadosNovoContato['instituicao']);
+
+      $sql->execute();
+
+      
+  }
+
+  $dadosNovoContato=[];
   $erros=[];
+
   if(empty($_REQUEST) == false)
   {
       $request = array_map('trim', $_REQUEST);
@@ -13,7 +33,8 @@
           'usuario' => FILTER_DEFAULT,
           'email' => FILTER_VALIDATE_EMAIL,
           'senha' => FILTER_DEFAULT,
-          'confsenha' => FILTER_DEFAULT
+          'confsenha' => FILTER_DEFAULT,
+          'instituicao' => FILTER_DEFAULT
         ]
       );
 
@@ -22,43 +43,55 @@
     {$erros[] = "O campo Nome precisa ser preenchido.";}
     else if (strlen($nome) > 20)
     {$erros[] = "O nome informado não pode ultrapassar 20 caracteres.";}
+    $dadosNovoContato['nome'] = $nome;
 
     $sobrenome= $request['sobrenome'];
     if ($sobrenome == false)
     {$erros[] = "O campo Sobrenome precisa ser preenchido.";}
     else if (strlen($sobrenome) > 20)
     {$erros[] = "O sobrenome informado não pode ultrapassar 20 caracteres.";}
+    $dadosNovoContato['sobrenome'] = $sobrenome;
 
     $usuario= $request['usuario'];
     if ($usuario == false)
     {$erros[] = "O campo Usuário precisa ser preenchido.";}
     else if (strlen($usuario) < 3 || strlen($usuario) > 20)
     {$erros[] = "O nome de usuário informado não pode ultrapassar 20 caracteres.";}
+    $dadosNovoContato['usuario'] = $usuario;
 
     $email= $request['email'];
     if ($email == false)
     {$erros[] = "O campo Email precisa ser preenchido.";}
+    $dadosNovoContato['email'] = $email;
 
     $senha = $request['senha'];
     if ($senha == false)
     {$erros[] = "O campo Senha precisa ser preenchido.";}
     else if (strlen($senha) < 6 || strlen($senha) > 12)
     {$erros[] = "A senha informada não é válida.";}
-    else
-    {$erros[] = "Senha confirmada com sucesso.";}
+    $dadosNovoContato['senha'] = $senha;
 
-    $confirmaSenha= $request['confirmaSenha'];
+    $confirmaSenha= $request['confsenha'];
     if ($confirmaSenha == false)
     {$erros[] = "O campo Confirme Sua Senha precisa ser preenchido.";}
     else if ($confirmaSenha!=$senha)
     {$erros[] = "A senha informada não é válida.";}
-    else
-    {$erros[] = "Senha confirmada com sucesso.";}
 
+    $instituicao= $request['instituicao'];
+    if ($instituicao == false)
+    {$erros[] = "O campo Instituição precisa ser preenchido.";}
+    else if (strlen($instituicao) > 20)
+    {$erros[] = "A instituição informada não pode ultrapassar 20 caracteres.";}
+    $dadosNovoContato['instituicao'] = $instituicao;
   }
 
   foreach ($erros as $x) {
     echo  $x;
+  }
+
+  if (empty($erros)) {
+    echo 'Usuário inserido com sucesso';
+    InsereUsuario($dadosNovoContato);
   }
 
 ?>
