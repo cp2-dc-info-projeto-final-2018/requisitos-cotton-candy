00fb13031ac6cf29c67fd47fccbb5e3918e63c4d
LeFilgueiras <filgueiras.leticiac@gmail.com>
2018-11-19 20:56:57 -0200

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -1,25 +1,30 @@
 USE [ProjetoFinal];
 
-   CREATE TABLE Usuario(
+      CREATE TABLE Usuario(
     	id_Aluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     	Nome VARCHAR(100) NOT NULL,
       Sobrenome VARCHAR(100) NOT NULL,
     	Email VARCHAR(50) NOT NULL,
     	Usuario VARCHAR(30) NOT NULL,
     	Senha VARCHAR(60) NOT NULL,
-    	Instituicao VARCHAR(50));
+    	Instituicao VARCHAR(50))
+      Foto VARCHAR(100);
 
     CREATE TABLE Materia(
     	id_Materia INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     	Nome VARCHAR(100) NOT NULL,
-    	Cor VARCHAR(7) NOT NULL,
     	id_Aluno INT NOT NULL,
       FOREIGN KEY (id_Aluno) REFERENCES Usuario(id_Aluno));
 
     CREATE TABLE Avaliacao(
     	id_Avaliacao INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    	Tipo VARCHAR(100) NOT NULL,
-      Valor FLOAT,
+      certificacao INT,
+      teste FLOAT,
+      trab1 FLOAT,
+      trab2 FLOAT,
+      trab3 FLOAT,
+      prova FLOAT,
+      apoio FLOAT,
       id_Aluno INT NOT NULL,
       id_Materia INT NOT NULL,
       FOREIGN KEY (id_Aluno) REFERENCES Usuario(id_Aluno),
